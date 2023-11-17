24ab4bc152ef132549db1b0121bc9acedfb5aa45
VladyslavTodo <124445034+VladyslavTodo@users.noreply.github.com>
2023-11-17 22:47:48 +0200

    fix

--- src/components/ContactForm/ContactsForm.jsx
+++ src/components/ContactForm/ContactsForm.jsx
@@ -16,12 +16,11 @@ const ContacsForm = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  const handleChangeName = e => {
-    setName(e.target.value);
-  };
+  const handleChange = event => {
+    const { name, value } = event.target;
 
-  const handleChangeNumber = e => {
-    setNumber(e.target.value);
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
   };
 
   const handleSubmitContacts = evt => {
@@ -56,7 +55,7 @@ const ContacsForm = () => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           value={name}
-          onChange={handleChangeName}
+          onChange={handleChange}
           required
         />
       </Label>
@@ -68,7 +67,7 @@ const ContacsForm = () => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           value={number}
-          onChange={handleChangeNumber}
+          onChange={handleChange}
           required
         />
       </Label>
